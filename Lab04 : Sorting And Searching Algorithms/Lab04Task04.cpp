42fc65806ecc8e6329032da41124ede8f408ab8d
Yash Raj <yashmandhan608@gmail.com>
2025-09-14 13:41:11 +0500

    Update Lab04Task04.cpp

--- Lab04 : Sorting And Searching Algorithms/Lab04Task04.cpp
+++ Lab04 : Sorting And Searching Algorithms/Lab04Task04.cpp
@@ -2,6 +2,23 @@
 #include<iostream>
 using namespace std;
 
+int binarySearch(int arr[], int size, int key){
+    int start = 0;
+    int end = size- 1;
+    while(start <= end){
+        int mid = start +(end -start)/2;
+          if(arr[mid]==key){
+            return mid;
+          }
+          if(arr[mid]<key){
+            start = mid +1;
+          }else{
+            end = mid-1;
+          }
+    }
+    return -1;
+}
+
 void InsertionSort(int arr[], int size){
 for(int i =1;i<size;i++){
     int current = arr[i];
@@ -39,5 +56,16 @@ int main(){
    cout<<endl;
    cout<<"After sorting: "<<endl;
    display(arr,size);
+   cout<<endl;
+   cout<<"Enter the number you want to search in array"<<endl;
+   int key;
+   cin>>key;
+   int result = binarySearch(arr,size,key);
+   if(result){
+    cout<<"Key Found"<<endl;
+   }else{
+    cout<<"Key not found"<<endl;
+   }
+
     return 0;
 }
